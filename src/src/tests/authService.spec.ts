6417e8d3164c06670f1d2797ec95f05ed8fee606
codethi <docencia.thiago@gmail.com>
2023-03-15 20:33:35 -0300

    feat: test error cases auth user

--- src/src/tests/authService.spec.ts
+++ src/src/tests/authService.spec.ts
@@ -3,7 +3,6 @@ import usersRepository from "repositories/usersRepository";
 import bcrypt from "bcrypt";
 import authRepository from "repositories/authRepository";
 import authServices from "services/authServices";
-import { v4 as uuid } from "uuid";
 
 jest.mock("uuid", () => ({ v4: () => "RandomString" }));
 
@@ -36,5 +35,49 @@ describe("Auth user", () => {
 
     expect(result).toHaveProperty("token");
     expect(result).toEqual({ token: "RandomString" });
+  });
+
+  it("should not be able to auth an nonexistent user", () => {
+    expect(async () => {
+      const user = {
+        id: 1,
+        name: "Name Test",
+        email: "email@test.com",
+        password: "testpass",
+      };
+
+      jest
+        .spyOn(usersRepository, "getUserByEmailRepository")
+        .mockImplementationOnce((): any => {
+          return undefined;
+        });
+
+      await authServices.signinService(user.email, user.password);
+    }).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should not be able to auth an wrong password", () => {
+    expect(async () => {
+      const user = {
+        id: 1,
+        name: "Name Test",
+        email: "email@test.com",
+        password: "testpass",
+      };
+
+      jest
+        .spyOn(usersRepository, "getUserByEmailRepository")
+        .mockImplementationOnce((): any => {
+          return [user];
+        });
+
+      const bcryptCompare = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve(false));
+
+      (bcrypt.compare as jest.Mock) = bcryptCompare;
+
+      await authServices.signinService(user.email, user.password);
+    }).rejects.toBeInstanceOf(Error);
   });
 });
